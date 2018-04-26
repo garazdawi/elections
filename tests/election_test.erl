8e18f4bbd987c82ad0f72846a8490039057a1448
Lukas Larsson <lukas@erlang-solutions.com>
2018-04-26 11:07:37 +0200

    Update tests

--- tests/election_test.erl
+++ tests/election_test.erl
@@ -14,6 +14,9 @@ tests() ->
     [fun vote/0].
 
 vote() ->
-    ok = vote_chain:vote(123, "kalle anka", kommun),
-    ok = vote_chain:vote(124, "kalle anka", kommun).
-
+    ok = vote_chain:vote(123, "kalle anka", riksdag),
+    ok = vote_chain:vote(124, "kalle anka", riksdag),
+    #{"kalle anka" := 2} = vote_chain:count_votes(riksdag, undefined),
+    ok = vote_chain:vote(124, "kalle anka", riksdag),
+    #{"kalle anka" := 2} = vote_chain:count_votes(riksdag, undefined),
+    ok.
